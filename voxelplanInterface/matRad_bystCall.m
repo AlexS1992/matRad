c7603ef95b2493dc7a4afe3dd526506a4bc7e109
Alex Stadler <alexstadler92@gmail.com>
2015-12-09 21:17:00 +0100

    make pln input optional

--- voxelplanInterface/matRad_bystCall.m
+++ voxelplanInterface/matRad_bystCall.m
@@ -6,10 +6,22 @@ function matRad_bystCall(patientID,pln)
 [ct,cst] = matRad_importVirtuosDataSet(patientID,0);
 
 %%
-load(pln)
-
-if ~isfield(pln, 'isoCenter')
-    pln.isoCenter = matRad_getIsoCenter(cst,ct,0);
+if nargin == 1
+    pln.SAD             = 10000; %[mm]
+    pln.isoCenter       = matRad_getIsoCenter(cst,ct,0);
+    pln.bixelWidth      = 5; % [mm] / also corresponds to lateral spot spacing for particles
+    pln.gantryAngles    = [30]; % [°]
+    pln.couchAngles     = [0]; % [°]
+    pln.radiationMode   = 'protons'; % either photons / protons / carbon
+    pln.bioOptimization = 'none'; % none: physical optimization; effect: effect-based optimization; RBExD: optimization of RBE-weighted dose
+    pln.numOfFractions  = 30;
+    pln.runSequencing   = false; % 1/true: run sequencing, 0/false: don't / will be ignored for particles and also triggered by runDAO below
+    pln.runDAO          = false; % 1/true: run DAO, 0/false: don't / will be ignored for particles
+else
+    load(pln)
+    if ~isfield(pln, 'isoCenter')
+        pln.isoCenter = matRad_getIsoCenter(cst,ct,0);
+    end
 end
 
 pln.numOfBeams      = numel(pln.gantryAngles);
